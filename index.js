c47dffeed2f9dee00d304ff05ffd82199be3da21
Robert Hitchcock <robert.e.hitchcock@gmail.com>
2023-02-13 14:34:55 -0500

    javaScript start

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 fetch( 'http://localhost:3000/information' )
 .then( r => r.json() )
 .then( musicArray => {
-    console.log( musicArray )
+    renderMusic( musicArray )
 })
+
+// const conatiner = document.querySelector("#myCarousel > div")
+// const liconatiner = document.querySelector("#myCarousel > ol")
+
+function renderMusic( musicArray ) {
+
+ musicArray.forEach( music => {
+   console.log(music)
+   const conatiner = document.querySelector("#myCarousel > div")
+    const olConatiner = document.querySelector("#myCarousel > ol")
+    const newDiv = document.createElement("div")
+    newDiv.className = "New-Div"
+    conatiner.append(newDiv)
+    const h2 = document.createElement("h2")
+    h2.textContent = music.Genre
+    const image = document.createElement("img")
+    image.src = music.image
+    const h3 = document.createElement("h3")
+    h3.textContent = music.artists
+    const p = document.createElement("p")
+    p.textContent = music.message
+    newDiv.append(h2, image, h3, p)
+    const li = document.createElement("li")
+    li.dataTarget = "#myCarousel"
+   olConatiner.append(li)
+
+
+    })
+   
+
+
+}
