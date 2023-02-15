ed00d1198651c4663b4fcd0203fc0a0be8ca2857
Robert Hitchcock <robert.e.hitchcock@gmail.com>
2023-02-15 10:04:20 -0500

    JAVASCIPT Clean UP

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
+//Mouse Over/Out Events
 document.getElementById("titleH1").addEventListener("mouseover", mouseOver);
 document.getElementById("titleH1").addEventListener("mouseout", mouseOut);
-// document.getElementById("p").addEventListener("mouseover", mouseOver);
-// document.getElementById("p").addEventListener("mouseout", mouseOut);
 
 function mouseOver() {
     document.getElementById("titleH1").style.color = "purple";
-    // document.getElementById("p").style.color = "#990000";
-
 }
   
 function mouseOut() {
     document.getElementById("titleH1").style.color = "grey";
-//     document.getElementById("p").style.color = "black";
- }
+}
+
+// Creating Cards
 const button = document.createElement("button")
+const container = document.querySelector("body > div")
+const newDiv = document.querySelector(".info")
 
 fetch( 'http://localhost:3000/information' )
 .then( r => r.json() )
@@ -23,54 +23,46 @@ fetch( 'http://localhost:3000/information' )
     })
 })
 
-// const conatiner = document.querySelector("#myCarousel > div")
-// const liconatiner = document.querySelector("#myCarousel > ol")
 
 function renderMusic( music ) {
-   //console.log(music)
-   const container = document.querySelector("body > div")
-    //const olConatiner = document.querySelector("#myCarousel > ol")
-    //newDiv.className = "container"
-    //container.append(newDiv)
-    const card = document.createElement('div')
-    card.className = "card"
-    container.append(card)
-    const h3 = document.createElement("h3")
+   //Caraoel Elements 
+   const card = document.createElement('div')
+   card.className = "card"
+   container.append(card)
+
+   const h3 = document.createElement("h3")
     h3.className = "title"
     h3.textContent = music.Genre
+
     const image = document.createElement("img")
     image.src = music.image
     image.alt = music.Genre
 
     card.append(h3, image)
     
-    
     card.addEventListener('click', () => {
-      
-      const newDiv = document.querySelector(".info")
-      const h4 = document.querySelector(".h4")
-      h4.textContent = "Genre Top Artists: " + music.artists
-      const p = document.querySelector(".p")
-      p.textContent = music.message
-      
-      button.textContent = parseInt(music.likes) + " Likes \u2665"
-      newDiv.append(h4, p, button)
 
-    
-      
-      
-    })
-  }
-    
+    //Elemnts appear Below Once Clicked   
+    const h4 = document.querySelector(".h4")
+    h4.textContent = "Genre Top Artists: " + music.artists
+    const p = document.querySelector(".p")
+    p.textContent = music.message
 
+    //Button Likes
+    button.textContent = parseInt(music.likes) + " Likes \u2665"
+    newDiv.append(button)
+
+  })
+}
     
+//Like button Listener    
 button.addEventListener("click", () => {
     button.textContent = parseInt(button.textContent) + 1 + " Likes \u2665"
 
-    })
+})
 
 
-
+//Form 
 const theForm = document.querySelector("#theForm")
 theForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -82,7 +74,7 @@ theForm.addEventListener("submit", (e) => {
       message: e.target.fdesc.value,
       likes: e.target.vol.value
     }
-    
+    //POST Request
     fetch( 'http://localhost:3000/information', {
       method: "POST",
       headers: {"Content-Type": "application/json"},
@@ -92,7 +84,7 @@ theForm.addEventListener("submit", (e) => {
     .then( addNewMusicObj => {
       renderMusic( addNewMusicObj )
     })
-    //renderMusic( addNewMusicObj )
+   
 })
 
 
